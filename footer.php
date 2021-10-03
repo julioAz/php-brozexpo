5be53ba89c2749effa8b1b8ab2cde5d8dab34f44
Julio Azevedo <cerebros.developer@gmail.com>
2021-10-03 03:10:08 -0300

    Criação do repositorio para projeto brozexpo

---
+++ footer.php
@@ -0,0 +1,196 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="d-none">Nossos contatos</h2>
+                    <ul class="list-unstyled"> 
+                        <li>
+                            <a class="navbar-brand" href="/">
+                                <h3 class="d-none">BRZ expo</h3>
+                                <img src="img/logo.png" alt="Logo">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tel:5521982242190">
+                                <i class="fas fa-phone"></i>
+                                <h3>Call us</h3>
+                                <p>+55 21 982 242 190</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:contato@brzexpo.com.br">
+                                <i class="fas fa-envelope"></i>
+                                <h3>Email</h3>
+                                <p>contato@brzexpo.com.br</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <h3>Based</h3>
+                                <p>Av. Passos, 115 - 1.416
+                                    <br>Rio de Janeiro, RJ - Brazil
+                                    <br>ZIP 20051-040
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container-copyright">
+            <div class="container">
+                <div class="col-12 col-md-8 offset-md-2 text-center">
+                    <p>© 2021 - BRZ Expo is a trademark of Vitrine Brasil Internacional Comercial Exportadora LTDA. – Rio de Janeiro / Brazil</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <div class="modal fade" id="cafeAurea" tabindex="-1" aria-labelledby="cafeAurea" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content container p-0">
+                <div class="modal-header">
+                    <a class="navbar-brand" href="/">
+                        <img src="img/logo.png" alt="Logo">
+                    </a>
+                    <button type="button" class="btn-exit" data-bs-dismiss="modal">X</button>
+                    <img class="img-logo" src="https://via.placeholder.com/356x47" alt="Café Aurea">
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-1">
+                            <h2>High quality Brazilian food products.</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12 col-lg-7 offset-lg-1">
+                            <h3>We are committed to our international customers to offer high quality products, which respect the best environmental, administrative and social practices.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum ipsum quis eros convallis consequat. Maecenas sed purus eu dolor fringilla malesuada in quis enim. Phasellus eu dictum augue. Aenean sit amet pretium est. </p>
+                            <p>Vestibulum ex nisi, pharetra sollicitudin fringilla non, dignissim eu diam. Cras fermentum tortor est, sit amet finibus justo accumsan sit amet. Vestibulum et neque sed purus sagittis accumsan eu at tellus. Cras at pharetra nibh.  </p>
+                            <p>Morbi at enim volutpat, volutpat est quis, ultrices quam. Vivamus tristique massa at est maximus fringilla non ut elit. Curabitur rhoncus nec sem et finibus. Vivamus libero magna, lobortis sit amet consequat et, viverra at nibh. Donec ex lectus, molestie ac felis vitae, dictum faucibus odio.</p>
+                            <p>Nunc rutrum, metus vel euismod varius, tellus purus bibendum ante, in blandit orci dolor in lorem. Pellentesque laoreet, dolor nec dictum tincidunt, turpis nunc ultrices magna, vitae molestie augue mauris a libero. Donec nibh ipsum, aliquam vitae aliquam nec, ultrices at velit. </p>
+                            <a class="btn btn-primary btn-brz" href="#">Download catalogue</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="vitao" tabindex="-1" aria-labelledby="vitao" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content container p-0">
+                <div class="modal-header">
+                    <a class="navbar-brand" href="/">
+                        <img src="img/logo.png" alt="Logo">
+                    </a>
+                    <button type="button" class="btn-exit" data-bs-dismiss="modal">X</button>
+                    <img class="img-logo" src="https://via.placeholder.com/356x47" alt="Café Aurea">
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-1">
+                            <h2>High quality Brazilian food products.</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12 col-lg-7 offset-lg-1">
+                            <h3>We are committed to our international customers to offer high quality products, which respect the best environmental, administrative and social practices.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum ipsum quis eros convallis consequat. Maecenas sed purus eu dolor fringilla malesuada in quis enim. Phasellus eu dictum augue. Aenean sit amet pretium est. </p>
+                            <p>Vestibulum ex nisi, pharetra sollicitudin fringilla non, dignissim eu diam. Cras fermentum tortor est, sit amet finibus justo accumsan sit amet. Vestibulum et neque sed purus sagittis accumsan eu at tellus. Cras at pharetra nibh.  </p>
+                            <p>Morbi at enim volutpat, volutpat est quis, ultrices quam. Vivamus tristique massa at est maximus fringilla non ut elit. Curabitur rhoncus nec sem et finibus. Vivamus libero magna, lobortis sit amet consequat et, viverra at nibh. Donec ex lectus, molestie ac felis vitae, dictum faucibus odio.</p>
+                            <p>Nunc rutrum, metus vel euismod varius, tellus purus bibendum ante, in blandit orci dolor in lorem. Pellentesque laoreet, dolor nec dictum tincidunt, turpis nunc ultrices magna, vitae molestie augue mauris a libero. Donec nibh ipsum, aliquam vitae aliquam nec, ultrices at velit. </p>
+                            <a class="btn btn-primary btn-brz" href="#">Download catalogue</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="britivic" tabindex="-1" aria-labelledby="britivic" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content container p-0">
+                <div class="modal-header">
+                    <a class="navbar-brand" href="/">
+                        <img src="img/logo.png" alt="Logo">
+                    </a>
+                    <button type="button" class="btn-exit" data-bs-dismiss="modal">X</button>
+                    <img class="img-logo" src="https://via.placeholder.com/356x47" alt="Café Aurea">
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-1">
+                            <h2>High quality Brazilian food products.</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12 col-lg-7 offset-lg-1">
+                            <h3>We are committed to our international customers to offer high quality products, which respect the best environmental, administrative and social practices.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum ipsum quis eros convallis consequat. Maecenas sed purus eu dolor fringilla malesuada in quis enim. Phasellus eu dictum augue. Aenean sit amet pretium est. </p>
+                            <p>Vestibulum ex nisi, pharetra sollicitudin fringilla non, dignissim eu diam. Cras fermentum tortor est, sit amet finibus justo accumsan sit amet. Vestibulum et neque sed purus sagittis accumsan eu at tellus. Cras at pharetra nibh.  </p>
+                            <p>Morbi at enim volutpat, volutpat est quis, ultrices quam. Vivamus tristique massa at est maximus fringilla non ut elit. Curabitur rhoncus nec sem et finibus. Vivamus libero magna, lobortis sit amet consequat et, viverra at nibh. Donec ex lectus, molestie ac felis vitae, dictum faucibus odio.</p>
+                            <p>Nunc rutrum, metus vel euismod varius, tellus purus bibendum ante, in blandit orci dolor in lorem. Pellentesque laoreet, dolor nec dictum tincidunt, turpis nunc ultrices magna, vitae molestie augue mauris a libero. Donec nibh ipsum, aliquam vitae aliquam nec, ultrices at velit. </p>
+                            <a class="btn btn-primary btn-brz" href="#">Download catalogue</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="contatoModal" tabindex="-1" aria-labelledby="contatoModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content container p-0">
+                <div class="modal-header">
+                    <a class="navbar-brand" href="/">
+                        <img src="img/logo.png" alt="Logo">
+                    </a>
+                    <button type="button" class="btn-exit" data-bs-dismiss="modal">X</button>
+                    <div class="col-12 col-md-8 offset-md-2 text-center">
+                        <h2>Let's work together on your next import project.</h2>
+                        <form class="col-12 col-md-10 offset-md-1" action="email.php" method="post">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Name: </label>
+                            <input type="text" class="form-control" name="nome" for="nome" placeholder="Your Name ..." id="name" aria-describedby="nameHelp" required=""></div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email:</label>
+                            <input type="email" for="email" name="email" class="form-control" placeholder="Your email ..." id="exampleInputEmail1" aria-describedby="emailHelp" required=""></div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Message:</label>
+                            <textarea class="form-control" for="message" name="msg" id="exampleFormControlTextarea1" placeholder="Please add your message here..." required="" rows="6"></textarea></div>
+                            <button type="submit" class="btn btn-primary w-100 btn-brz">Send message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="contatoModalSucesso" tabindex="-1" aria-labelledby="contatoModalSucesso" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content container p-0">
+                <div class="modal-header">
+                    <a class="navbar-brand" href="/">
+                        <img src="img/logo.png" alt="Logo">
+                    </a>
+                    <button type="button" class="btn-exit" data-bs-dismiss="modal">X</button>
+                    <div class="col-12 col-md-8 offset-md-2 text-center">
+                        <h2>Let's work together on your next import project.</h2>
+                        <div class="sucess-form">
+                            <i class="far fa-check-circle"></i>
+                            <p>Registration performed successfully</p>
+                        </div>
+                        <a class="btn btn-primary btn-brz" href="#">Download catalogue</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="/js/script.js"></script>
+</html>
